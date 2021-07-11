48db4a2f285c7b6c0c797d0f7c8547525395e38a
sehaz-1021 <79392785+sehaz-1021@users.noreply.github.com>
2021-07-11 14:50:02 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,18 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            'isStatic': true,
+        }
+        this.body = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        this.image = loadImage("images/walking_1.png","images/walking_2.png","images/walking_3.png","images/walking_4.png","images/walking_5.png","images/walking_6.png","images/walking_7.png","images/walking_8.png");
+        World.add(world, this.body)
+    }
+
+    display(){
+        var pos = this.body.position;
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y+70, 300, 300);
+    }
+}
+
